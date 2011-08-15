07f5ab428df15817b5abc19e7342ec23ad02da53
mstandio <mstandio@o2.pl>
2011-08-15 15:44:11 +0200

    init commit

---
+++ stax_basics/src/stax_basics/Stax_basics.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stax_basics;
+
+/**
+ *
+ * @author Marek
+ */
+public class Stax_basics {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
